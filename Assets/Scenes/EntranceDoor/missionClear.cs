3428b5619ba76622d3ae195f5321cb2190590a80
ihj0303 <89908402+ihj0303@users.noreply.github.com>
2022-06-07 07:20:27 +0900

    EwhaLogo

---
+++ Assets/Scenes/EntranceDoor/missionClear.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class missionClear : MonoBehaviour
+{
+    public GameObject curve1;
+    public GameObject curve2;
+    public GameObject curve3;
+
+    public GameObject text1;
+    public GameObject text2;
+    public GameObject button;
+    public GameObject image;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((curve1.GetComponent<MeshRenderer>().material.color == Color.green) ||
+            (curve2.GetComponent<MeshRenderer>().material.color == Color.green) ||
+            (curve3.GetComponent<MeshRenderer>().material.color == Color.green))
+
+            image.SetActive(false);
+
+        if ((curve1.GetComponent<MeshRenderer>().material.color == Color.green) && 
+            (curve2.GetComponent<MeshRenderer>().material.color == Color.green) && 
+            (curve3.GetComponent<MeshRenderer>().material.color == Color.green))
+        {
+            text1.SetActive(false);
+            text2.SetActive(true);
+            button.SetActive(true);
+        }
+        
+    }
+}
---
+++ Assets/Scenes/EntranceDoor/raycast.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycast : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                hit.transform.gameObject.GetComponent<MeshRenderer>().material.color = new Color(0,1,0,1);
+            }
+        }
+
+    }
+}
