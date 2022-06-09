650b6d5a1d8e7b718041ff97413e79571c8f886d
s0ooo0k <izz_m2@naver.com>
2022-06-09 18:30:27 +0900

    [feat] all scene UI

---
+++ Assets/Scenes/Main/Script/GoPodo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoPodo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void NextScene()
+    {
+        SceneManager.LoadScene(18);
+    }
+}
---
+++ Assets/Scenes/Posco/PoscoQr/Scripts/PoscoButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PoscoButton : MonoBehaviour
+{
+    public GameObject Bubble;
+    public Button btnNext;
+    void Start()
+    {
+        Bubble.SetActive(false);
+        btnNext.onClick.AddListener(showBubble);
+    }
+
+    void showBubble(){
+        Bubble.SetActive(true);
+    }
+}
