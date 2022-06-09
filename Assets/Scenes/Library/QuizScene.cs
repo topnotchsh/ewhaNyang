1c9601b10dd139950fa4898ed96c3d208b8e97f8
s0ooo0k <izz_m2@naver.com>
2022-06-09 22:02:41 +0900

    [feat] library UI

---
+++ Assets/Scenes/Library/QuizScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class QuizScene : MonoBehaviour
+{
+    public GameObject Quiz1;
+    public GameObject Quiz2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Quiz2.SetActive(false);
+    }
+
+    // Update is called once per frame
+    public void Pass1()
+    {
+        Quiz1.SetActive(false);
+        Quiz2.SetActive(true);
+    }
+
+    public void Pass2()
+    {
+        SceneManager.LoadScene(23);
+    }
+}
