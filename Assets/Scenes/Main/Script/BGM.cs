03f7453e92425ab1fc557e540af9cfee8da81d1f
ihj0303 <89908402+ihj0303@users.noreply.github.com>
2022-06-10 03:29:26 +0900

    bgmbase

---
+++ Assets/Scenes/Main/Script/BGM.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    public static BGM instance = null;
+
+    public AudioClip Caketown;
+
+    void Start()
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (instance != this)
+                Destroy(this.gameObject);
+        }
+
+        audio.clip = Caketown;
+
+        audio.Play();
+
+
+    }
+
+
+
+}
