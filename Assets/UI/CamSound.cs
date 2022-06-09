d2d13bd667c5c4635b1fafe401af8d2252940b54
topnotchsh <dltngus00@ewhain.net>
2022-06-10 04:05:05 +0900

    버튼음+자잘 수정

---
+++ Assets/UI/CamSound.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSound : MonoBehaviour
+{
+    public AudioSource camSound;
+
+    public void onCamSound()
+    {
+    	camSound.Play();
+    }
+}
